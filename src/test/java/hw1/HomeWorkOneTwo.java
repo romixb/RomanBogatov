5602206fc5daaee83280661ab654965cb6b5d540
Roman_Bogatov1 <roman_boagtov1@epam.com>
2018-12-28 15:54:13 +0300

    Added:hw2 - Commented out Before/after Method Moved ChromeDriver instantiation inside each @Test

--- src/test/java/hw1/HomeWorkOneTwo.java
+++ src/test/java/hw1/HomeWorkOneTwo.java
@@ -19,8 +19,6 @@ public class HomeWorkOneTwo {
 
     private ChromeDriver driver;
 
-    private  SoftAssert softAssert;
-
     private static final String TARGET_URL = "https://epam.github.io/JDI/";
 
 
@@ -87,7 +85,7 @@ public class HomeWorkOneTwo {
     @Test(groups = {"Smoke"})
     public void testMethod() {
 
-        softAssert = new SoftAssert();
+        SoftAssert softAssert = new SoftAssert();
 
         driver.navigate().to(TARGET_URL);
 
@@ -170,178 +168,10 @@ public class HomeWorkOneTwo {
 //    16.Assert that there is Footer
         WebElement footer = driver.findElement(By.cssSelector(".footer-content"));
         assertTrue(footer.isDisplayed());
-    }
-    @Test(groups = {"Smoke"})
-    public void testMethod2() {
-        driver.navigate().to(TARGET_URL);
-
-//    2. Assert Browser title
-        assertEquals(driver.getTitle(), EXPECTED_TITLE);
-
-//    3.Perform login
-        driver.findElement(By.cssSelector(".profile-photo")).click();
-        driver.findElement(By.cssSelector("#Name")).sendKeys(TEST_USER);
-        driver.findElement(By.cssSelector("#Password")).sendKeys(TEST_PASSWORD);
-        driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
-
-//    4.Assert User name in the left-top side of screen that user is logged
-        WebElement userNameElement = driver.findElement(By.cssSelector(".profile-photo span"));
-        assertEquals(userNameElement.getText(), EXPECTED_USER_NAME);
-
-//    5. Assert Browser title
-        assertEquals(driver.getTitle(), EXPECTED_TITLE);
-
-//    6.Assert that there are 4 items on the header section are displayed and they have proper texts
-        List<WebElement> navElems = driver.findElements(By.cssSelector(".navbar-nav.m-l8 > li"));
-        assertEquals(navElems.size(), EXPECTED_NAV_SIZE);
-        assertEquals(navElems.get(0).getText(), EXPECTED_NAV_BAR_TEXT.get(0));
-        assertEquals(navElems.get(1).getText(), EXPECTED_NAV_BAR_TEXT.get(1));
-        assertEquals(navElems.get(2).getText(), EXPECTED_NAV_BAR_TEXT.get(2));
-        assertEquals(navElems.get(3).getText(), EXPECTED_NAV_BAR_TEXT.get(3));
-
-//    7. Assert that there are 4 images on the Index Page and they are displayed
-        WebElement pageImg = driver.findElement(By.cssSelector(".icon-practise"));
-        assertTrue(pageImg.isDisplayed());
-
-        pageImg = driver.findElement(By.cssSelector(".icon-custom"));
-        assertTrue(pageImg.isDisplayed());
-
-        pageImg = driver.findElement(By.cssSelector(".icon-multi"));
-        assertTrue(pageImg.isDisplayed());
-
-        pageImg = driver.findElement(By.cssSelector(".icon-base"));
-        assertTrue(pageImg.isDisplayed());
-
-//    8. Assert that there are 4 texts on the Index Page under icons and they have proper text
-        List<WebElement> webElement = driver.findElements(By.cssSelector(".clerafix > .col-sm-3"));
-
-        assertEquals(webElement.get(0).getText(), EXPECTED_TEXT.get(0));
-        assertEquals(webElement.get(1).getText(), EXPECTED_TEXT.get(1));
-        assertEquals(webElement.get(2).getText(), EXPECTED_TEXT.get(2));
-        assertEquals(webElement.get(3).getText(), EXPECTED_TEXT.get(3));
-
-//    9.Assert a text of the main headers
-        WebElement mainTitle = driver.findElement(By.cssSelector("[name='main-title']"));
-        assertEquals(mainTitle.getText(), EXPECTED_HEADER_TEXT.get(0));
-        mainTitle= driver.findElement(By.cssSelector("[name='jdi-text']"));
-        assertEquals(mainTitle.getText(), EXPECTED_HEADER_TEXT.get(1));
-
-//    10.Assert that there is the iframe in the center of page
-        WebElement iFrame = driver.findElement(By.id("iframe"));
-        assertTrue(iFrame.isDisplayed());
-
-//    11.Switch to the iframe and check that there is Epam logo in the left top conner of iframe
-        driver.switchTo().frame("iframe");
-        WebElement logo = driver.findElement(By.id("epam_logo"));
-        assertTrue(logo.isDisplayed());
-
-//    12.Switch to original window back
-        driver.switchTo().defaultContent();
-
-//    13.Assert a text of the sub header
-        WebElement subHeader = driver.findElement(By.cssSelector("h3 > [target = '_blank']"));
-        assertEquals(subHeader.getText(), EXPECTED_SUB_HEADER_TEXT);
-
-//    14.Assert that JDI GITHUB is a link and has a proper URL.Assert a text of the sub header
-        assertTrue(driver.findElement(By.linkText("JDI GITHUB")).isDisplayed());
-        assertEquals(driver.findElement(By.linkText("JDI GITHUB")).getAttribute("href"), EXPECTED_URL);
-
-//    15.Assert that there is Left Section
-        WebElement leftSection = driver.findElement(By.id("mCSB_1"));
-        assertTrue(leftSection.isDisplayed());
-
-//    16.Assert that there is Footer
-        WebElement footer = driver.findElement(By.cssSelector(".footer-content"));
-        assertTrue(footer.isDisplayed());
-    }
-    @Test(groups = {"Smoke"})
-    public void testMethod3() {
-        driver.navigate().to(TARGET_URL);
-
-//    2. Assert Browser title
-        assertEquals(driver.getTitle(), EXPECTED_TITLE);
-
-//    3.Perform login
-        driver.findElement(By.cssSelector(".profile-photo")).click();
-        driver.findElement(By.cssSelector("#Name")).sendKeys(TEST_USER);
-        driver.findElement(By.cssSelector("#Password")).sendKeys(TEST_PASSWORD);
-        driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
-
-//    4.Assert User name in the left-top side of screen that user is logged
-        WebElement userNameElement = driver.findElement(By.cssSelector(".profile-photo span"));
-        assertEquals(userNameElement.getText(), EXPECTED_USER_NAME);
-
-//    5. Assert Browser title
-        assertEquals(driver.getTitle(), EXPECTED_TITLE);
-
-//    6.Assert that there are 4 items on the header section are displayed and they have proper texts
-        List<WebElement> navElems = driver.findElements(By.cssSelector(".navbar-nav.m-l8 > li"));
-        assertEquals(navElems.size(), EXPECTED_NAV_SIZE);
-        assertEquals(navElems.get(0).getText(), EXPECTED_NAV_BAR_TEXT.get(0));
-        assertEquals(navElems.get(1).getText(), EXPECTED_NAV_BAR_TEXT.get(1));
-        assertEquals(navElems.get(2).getText(), EXPECTED_NAV_BAR_TEXT.get(2));
-        assertEquals(navElems.get(3).getText(), EXPECTED_NAV_BAR_TEXT.get(3));
-
-//    7. Assert that there are 4 images on the Index Page and they are displayed
-        WebElement pageImg = driver.findElement(By.cssSelector(".icon-practise"));
-        assertTrue(pageImg.isDisplayed());
-
-        pageImg = driver.findElement(By.cssSelector(".icon-custom"));
-        assertTrue(pageImg.isDisplayed());
-
-        pageImg = driver.findElement(By.cssSelector(".icon-multi"));
-        assertTrue(pageImg.isDisplayed());
-
-        pageImg = driver.findElement(By.cssSelector(".icon-base"));
-        assertTrue(pageImg.isDisplayed());
-
-//    8. Assert that there are 4 texts on the Index Page under icons and they have proper text
-        List<WebElement> webElement = driver.findElements(By.cssSelector(".clerafix > .col-sm-3"));
-
-        assertEquals(webElement.get(0).getText(), EXPECTED_TEXT.get(0));
-        assertEquals(webElement.get(1).getText(), EXPECTED_TEXT.get(1));
-        assertEquals(webElement.get(2).getText(), EXPECTED_TEXT.get(2));
-        assertEquals(webElement.get(3).getText(), EXPECTED_TEXT.get(3));
-
-//    9.Assert a text of the main headers
-        WebElement mainTitle = driver.findElement(By.cssSelector("[name='main-title']"));
-        assertEquals(mainTitle.getText(), EXPECTED_HEADER_TEXT.get(0));
-        mainTitle= driver.findElement(By.cssSelector("[name='jdi-text']"));
-        assertEquals(mainTitle.getText(), EXPECTED_HEADER_TEXT.get(1));
-
-//    10.Assert that there is the iframe in the center of page
-        WebElement iFrame = driver.findElement(By.id("iframe"));
-        assertTrue(iFrame.isDisplayed());
-
-//    11.Switch to the iframe and check that there is Epam logo in the left top conner of iframe
-        driver.switchTo().frame("iframe");
-        WebElement logo = driver.findElement(By.id("epam_logo"));
-        assertTrue(logo.isDisplayed());
-
-//    12.Switch to original window back
-        driver.switchTo().defaultContent();
-
-//    13.Assert a text of the sub header
-        WebElement subHeader = driver.findElement(By.cssSelector("h3 > [target = '_blank']"));
-        assertEquals(subHeader.getText(), EXPECTED_SUB_HEADER_TEXT);
-
-//    14.Assert that JDI GITHUB is a link and has a proper URL.Assert a text of the sub header
-
-        assertTrue(driver.findElement(By.linkText("JDI GITHUB")).isDisplayed());
-        assertEquals(driver.findElement(By.linkText("JDI GITHUB")).getAttribute("href"), EXPECTED_URL);
-
-//    15.Assert that there is Left Section
-        WebElement leftSection = driver.findElement(By.id("mCSB_1"));
-        assertTrue(leftSection.isDisplayed());
-
-//    16.Assert that there is Footer
-        WebElement footer = driver.findElement(By.cssSelector(".footer-content"));
-        assertTrue(footer.isDisplayed());
-
 
         softAssert.assertAll();
-
     }
+
     @AfterClass
     public void closeBrowser(){
        driver.close();
