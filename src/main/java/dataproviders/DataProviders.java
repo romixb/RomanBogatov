696a5e9a3885df40d94cedb7b564d8672a6a1ba9
Roman_Bogatov1 <roman_boagtov1@epam.com>
2018-12-28 12:52:20 +0300

    Added:checkIconTextDataProvider() to DataProviders, hw2.ex1.Exersise1

--- src/main/java/dataproviders/DataProviders.java
+++ src/main/java/dataproviders/DataProviders.java
@@ -12,4 +12,14 @@ public class DataProviders {
                 {"String3", 3}
         };
     }
+
+    @DataProvider
+    public Object[][] checkIconTextDataProvider(){
+        return new Object[][]{
+                {0, "To include good practices\nand ideas from successful\nEPAM project"},
+                {1, "To be flexible and\ncustomizable"},
+                {2, "To be multiplatform"},
+                {3, "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"}
+        };
+    }
 }
---
+++ src/test/java/hw2/ex1/Exercise1.java
@@ -0,0 +1,45 @@
+package hw2.ex1;
+
+import lesson2.ChromeDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import dataproviders.DataProviders;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class Exercise1 {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new ChromeDriverFactory().newDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.close();
+    }
+
+    @Test( dataProvider = "checkIconTextDataProvider", dataProviderClass = DataProviders.class)
+    public void checkTextIcon(int i, String s){
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+        List<WebElement> webElements = driver.findElements(By.cssSelector(".clerafix > .col-sm-3"));
+        assertEquals(webElements.get(i).getText(), s);
+
+
+    }
+}
